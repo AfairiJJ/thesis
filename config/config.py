1de8bd0550915e5a216d5857a42799d7352659b6
AfairiJJ <jan.janiszewski@afairi.io>
2023-05-25 12:06:37 +0200

    Changed function from leaky relu to sigmoid

--- config/config.py
+++ config/config.py
@@ -13,11 +13,12 @@ train_ganinput_noei = 'data/gan_dataprep/train_gan_noei.pickle'
 
 metadata = './config/metadata.json'
 
-dtnow = datetime.now().strftime('%Y%m%d%H')
+dtnow = datetime.now().strftime('%Y%m%d%H%M')
 output_generator = f'./data/generators/generator_{dtnow}.pt'
 output_discriminator = f'./data/discriminators/discriminator_{dtnow}.pt'
 output_loss = f'./data/losses/loss_{dtnow}.csv'
 
+print(dtnow)
 
 
 vars = [
@@ -35,7 +36,7 @@ cats_vars_xgb = [
              ]
 
 scaler_ei = MinMaxScaler # Cannot do different scaler since we need 0,1 (or we improve standard scaler to work!
-continuous_activation = nn.LeakyReLU
+continuous_activation = nn.Sigmoid
 
 num_samples = 500000
 num_features = 60
@@ -49,18 +50,19 @@ epochs = 15000
 batch_size = 128 # Article: 128, Code: 1500 (not good)
 disc_epochs = 2 # Both
 gen_epochs = 1 # Both
-loss_penalty = 1 # Article: 10, Code: 1
+loss_penalty = 0.1 # Article: 10, Code: 1
 gen_bn_decay = .9 # Article: 0.9, Code: 0.25
 disc_leaky_param = 0.2 # Both
 
 learning_rate = 0.01 # Article: 0.01, Code: 0.001 (not good)
 hiddens_gen = [100,100,100]
-hiddens_disc = [55, 55]
+hiddens_disc = [100, 100]
 round_with_beginning_set = -1
+show_plots_rounds = 50
 
-l2_regularization = 0.00001 # Article: 0, # Code: 0.1
+l2_regularization = 0 # Article: 0, # Code: 0.1
 gen_l2_regularization = l2_regularization
-noise_size = 60 # z-size, Code: 100
+noise_size = 100 # z-size, Code: 100
 critic = True
 disc_bn_decay = 0 if critic else 0.01 # Aritlce: 0.01, Code: 0.2
 
@@ -73,6 +75,3 @@ gen_learning_rate = learning_rate
 
 # Generator tuning
 cats_vars_gan = cats_vars_xgb + ['ClaimNb']
-
-
-
