4a530e40b967466fbf7c04d9f8d541c47670ed9a
AfairiJJ <jan.janiszewski@afairi.io>
2023-07-07 20:03:37 +0200

    aasdf

--- Functions/evaluation.py
+++ Functions/evaluation.py
@@ -1,9 +1,10 @@
 import sys
 
 import torch
+import operator
+
 from time import sleep
 from tqdm import tqdm
-from Functions.original import utils
 from Functions.original.methods.general.generator import Generator
 
 modelkey = 3
@@ -20,7 +21,7 @@ from sampler import sample
 import numpy as np
 import scipy.stats as stats
 
-def run_xgboost(train, test, version=1):
+def run_xgboost(train, test, seed, version=1):
     if version == 1:
         xgb_model = xgb.XGBRegressor(objective='count:poisson',
                                      n_estimators=1200,
@@ -30,7 +31,7 @@ def run_xgboost(train, test, version=1):
                                      subsample=0.9,
                                      min_child_weight=10,
                                      tree_method="hist",
-                                     random_state=1)
+                                     random_state=seed)
     elif version == 2:
         xgb_model = xgb.XGBRegressor(objective='count:poisson',
                                      n_estimators=500,
@@ -40,9 +41,9 @@ def run_xgboost(train, test, version=1):
                                      subsample=0.9,
                                      min_child_weight=10,
                                      tree_method="hist",
-                                     random_state=1)
+                                     random_state=seed)
 
-    train = train[test.columns]
+    train = train[test.columns]#.sample(frac=1, random_state=seed)
     xgb_model.fit(train.drop(labels='ClaimNb', axis=1), train['ClaimNb'])
 
     preds1 = xgb_model.predict(test.drop(labels='ClaimNb', axis=1))
@@ -88,7 +89,7 @@ def get_model(modelversion, datasets, ganruns=pd.read_csv('./config/ganruns.csv'
     return value
 
 
-def gan_bootstraped(value, testset, datasets, bootstraps, postprocess=False, version=1, seed=1):
+def gan_bootstraped(value, testset, datasets, bootstraps, postprocess=False, version=1, stratify=False):
     if value['has_ei']:
         metadata = cc.metadata_ei
         transformer = datasets['ei']['transformer']
@@ -99,26 +100,34 @@ def gan_bootstraped(value, testset, datasets, bootstraps, postprocess=False, ver
     devs = []
 
     for i in tqdm(range(0, bootstraps)):  # WARNING: Seed = i in this case (not as usual = 1)
-        torch.manual_seed(seed)
-        seed += 1
+        torch.manual_seed(i)
         testset = utils.resample(testset.copy(deep=True), replace=True, n_samples=len(testset), random_state=i,
-                              stratify=None)
-        preds1 = gan_xgb(metadata, postprocess, testset, transformer, value, version)
-        dev1 = metrics.poisson_deviance(preds1, testset['ClaimNb'])
+                              stratify=testset['ClaimNb'])
+        preds1 = gan_xgb(metadata, postprocess, testset, transformer, value, i, stratify, version)
+        dev1 = metrics.poisson_deviance(pred=preds1, obs=testset['ClaimNb'])
         devs += [dev1]
 
     return devs
 
 
-def gan_xgb(metadata, postprocess, testset, transformer, value, version=1):
+def gan_xgb(metadata, postprocess, testset, transformer, value, seed, stratify, version=1, supersample = False):
+    if stratify:
+        N = 200000
+    elif supersample:
+        N = 678013
+    else:
+        N = 100000
+
     train = sample(
         generator=value['generator'],
         num_features=metadata['num_features'],
-        num_samples=100000,
-        batch_size=100000,
+        num_samples=N,
+        batch_size=N,
         noise_size=int(value['z_size'])
     )
+
     train = pd.DataFrame(train)
+
     try:
         train = value['transformer'].inverse_transform(train)
     except AttributeError:
@@ -126,6 +135,7 @@ def gan_xgb(metadata, postprocess, testset, transformer, value, version=1):
     train[['VehPower', 'VehAge', 'DrivAge', 'BonusMalus']] = train[
         ['VehPower', 'VehAge', 'DrivAge', 'BonusMalus']].astype(int)
     # Data postprocessing
+
     if postprocess:
         train['ClaimNb'] = train['ClaimNb'].apply(lambda x: 4 if x > 4 else x)
         train['VehAge'] = train['VehAge'].apply(lambda x: 20 if x > 20 else x)
@@ -133,11 +143,28 @@ def gan_xgb(metadata, postprocess, testset, transformer, value, version=1):
         train['BonusMalus'] = train['BonusMalus'].apply(lambda x: 150 if x > 150 else int(x))
         train['VehPower'] = train['VehPower'].apply(lambda x: 9 if x > 9 else x)
     trainset = train.drop(train.filter(like="EI_", axis=1).columns, axis=1)
+    if not stratify:
+        trainset = trainset.drop(trainset.filter(like="Exposure", axis=1).columns, axis=1)
+
+    if stratify:
+        goal = 0.05326610225763612 # real data trainset mean
+        real = trainset['ClaimNb'].mean()
+        todrop = int(abs(real - goal) * len(trainset))
+        if real > goal:
+            idxs = trainset.loc[trainset['ClaimNb'] == 1].index
+        elif real < goal:
+            idxs = trainset.loc[trainset['ClaimNb'] == 0].index
+        drop_index = np.random.choice(idxs, todrop, replace=False, )
+        trainset = trainset.drop(index=drop_index, axis=1)
 
     # Fixing non-existent columns in the model
     missed_cols = [col for col in testset.columns if col not in train.columns]
     trainset[missed_cols] = 0
-    preds1 = run_xgboost(trainset, testset, version=version)
+    #print(f'Warning: Missing columns {missed_cols}')
+
+    if supersample:
+        return trainset
+    preds1 = run_xgboost(trainset, testset, version=version, seed=seed)
 
     return preds1
 
@@ -148,11 +175,11 @@ def xgb_bootstraped(trainset, testset, bootstraps, samplesize, version=1):
     for i in tqdm(range(0, bootstraps)):  # WARNING: Seed = i in this case (not as usual = 1)
         if samplesize > len(trainset):
             samplesize = len(trainset)
-        train = utils.resample(trainset, replace=True, n_samples=samplesize, random_state=i, stratify=None)
-        test = utils.resample(testset, replace=True, n_samples=len(testset), random_state=i, stratify=None)
-        preds1 = run_xgboost(train, test, version=version)
+        train = utils.resample(trainset.copy(deep=True), replace=True, n_samples=samplesize, random_state=i, stratify=trainset['ClaimNb'])
+        test = utils.resample(testset.copy(deep=True), replace=True, n_samples=len(testset), random_state=i, stratify=testset['ClaimNb'])
+        preds1 = run_xgboost(train, test, version=version, seed=i+100)
 
-        dev1 = metrics.poisson_deviance(preds1, testset['ClaimNb'])
+        dev1 = metrics.poisson_deviance(pred=preds1, obs=test['ClaimNb'])
 
         devs += [dev1]
 
